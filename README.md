940e38260eca89a615874120ca6a88af363b844d
Dan Halford <dan@sanx.org>
2023-09-18 23:48:10 +1000

    Documentation updates

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SanxLog
-Module to manage in-script logging
+# SanxLog - PowerShell Logging Module
+SanxLog is a PowerShell Core module that enables simple logging to file and to a growing number of time-series databases and log aggregation services; currently InfluxDB and Datadog.
+
+Once the module has been imported, use the `Set-Log` cmdlet to specify the logging target. At present, you can only log to one location at a time. Each log target also supports, if required, logging to the Console. Once the log type has been specified, to write log entries, simply call:
+`Write-Crit`
+`Write-Error`
+`Write-Warn`
+`Write-Info`
+`Write-Debug`
+
+## File logging
+Features include:
+* Log to any locally accessible path; local disk or UNC paths supported.
+* Configurable maximum log size, with automatic rollover
+* Timestamps can be local time or UTC
+
+### Log file format
+The log files are written in a human-readable (or at least, human parseable) format:
+\[Timestamp\] \[ProcessID\] \[Log level - CRIT|ERROR|WARN|INFO|DEBUG\] Message
+
+## InfluxDB logging
+Features include:
+* Supports InfluxDB on-premises and InfluxDB Cloud - InfluxDB v2.x currently supported and v3.x coming soon
+* Organisation name and bucket fully customiable with names and IDs supported
+* Entries tagged with a **Source**, **LogLevel**, **Hostname** and **ProcessID** tags for easy filtering
+
+## Datadog logging
+Features include:
+* **Service** fully configurable
+* **Source** and **LogLevel** included as log entry tags. Additional tags can be specified.
+
+Log messages are written in the following format:
+\[Log level - CRIT|ERROR|WARN|INFO|DEBUG\] \[ProcessID\] Message
+
+
--- module/Public/Configuration/Set-Log.ps1
+++ module/Public/Configuration/Set-Log.ps1
@@ -1,3 +1,68 @@
+<#
+.SYNOPSIS
+Specifies the type of logging to use, and the parameters for that logging type.
+#
+
+.DESCRIPTION
+This function is used to specify the type of logging to use, and the parameters for that logging type. It must be called before any logging functions are used.
+
+.PARAMETER LogType
+The type of logging to use. Must be one of 'File', 'InfluxDB', or 'Datadog'.
+
+.PARAMETER Path
+Applicable only when LogType is 'File'.
+The path to the log file. If the file does not exist, it will be created. If the file does exist, it will be appended to. If the file is larger than the MaxSize parameter, it will be renamed with a timestamp and a new file will be created. If the MaxSize parameter is not specified, the default is 10MB.
+
+.PARAMETER MaxSize
+Applicable only when LogType is 'File'.
+The maximum size of the log file in bytes. If the file is larger than this, it will be renamed with a timestamp and a new file will be created. If this parameter is not specified, the default is 10MB.
+
+.PARAMETER UniversalTime
+Applicable only when LogType is 'File'.
+If this switch is specified, timestamps in the log file will be written in UTC. If this switch is not specified, timestamps will be written in the local time zone.
+
+.PARAMETER ServerURL
+Applicable only when LogType is 'InfluxDB' or 'Datadog'.
+If LogType is Influx, specify the full URL to the InfluxDB server, including port number.
+For DataDog, specify the Datadog site URL.
+
+.PARAMETER Token
+Applicable only when LogType is 'InfluxDB'.
+The API token for InfluxDB authentication. The token will require, at a minimum, write access to the specified bucket.
+
+.PARAMETER Bucket
+Applicable only when LogType is 'InfluxDB'.
+The name or ID of the bucket to write logs to. The bucket must already exist.
+
+.PARAMETER OrgName
+Applicable only when LogType is 'InfluxDB'.
+The name or ID of the InfluxDB organisation. The organisation must already exist.
+
+.PARAMETER Source
+Applicable only when LogType is 'InfluxDB' or 'Datadog'.
+The name of the source for the logs. This will be used as a tag in Datadog and InfluxDB. Defaults to 'SanxLog'.
+
+.PARAMETER APIKey
+Applicable only when LogType is 'Datadog'.
+The API key for Datadog authentication.
+
+.PARAMETER Service
+Applicable only when LogType is 'Datadog'.
+The service name to tag log entries with.
+
+.PARAMETER Tags
+Applicable only when LogType is 'Datadog'.
+Metadata tags to add to log entries.
+
+.PARAMETER LogLevel
+The minimum log level to write to the log. Must be one of 'DEBUG', 'INFO', 'WARN', 'ERROR', or 'CRIT'.
+
+.PARAMETER DisplayLevel
+The minimum log level to display on the console. Must be one of 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRIT', or 'NONE'. If this parameter is not specified, the value of LogLevel will be used.
+
+.EXAMPLE
+Set-Log -LogType File -Path "C:\Logs\SanxLog.log" -MaxSize 10000000 -LogLevel INFO -DisplayLevel WARN
+#>
 function Set-Log() {
     param (
         [Parameter(Mandatory=$true, Position=0)]
